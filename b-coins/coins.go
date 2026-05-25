c0830072edd72baac180eae7d288111418a80c6c
Ayaka Hosoda <ayaka.hosoda@icloud.com>
2026-05-25 12:37:53 +0900

    b-coins

---
+++ b-coins/coins.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var A, B, C, X, count int
+	fmt.Scan(&A, &B, &C, &X)
+
+	for i := 0; i <= A; i++ {
+		for j := 0; j <= B; j++ {
+			for k := 0; k <= C; k++ {
+				if 500*i+100*j+50*k == X {
+					count++
+				}
+			}
+		}
+	}
+
+	fmt.Println(count)
+}
